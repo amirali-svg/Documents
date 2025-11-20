bb4e8d7ac2f6b142d0a2cd3d669bcbd85f2136ac
Amir <amiiraliborji1388@gmail.com>
2025-11-20 14:20:34 +0330

    .

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,2 +1,5 @@
-const speech = new SpeechSynthesisUtterance("Hello word");
-window.speechSynthesis.speak(speech);
+for (let i = 1; i <= 110; i++) {
+  fetch(`https://jsonplaceholder.typicode.com/todos/${i}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
