9ec80dacec8686ce8effcc44768dbf87a6c31432
Amir <amiiraliborji1388@gmail.com>
2025-11-20 10:11:30 +0330

    .

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,19 @@
-for (let i = 10; i <= 15; i++) {
-  console.log(i);
-  
-}
+const firstLogs = () => {
+  console.log("log 1");
+  console.log("log 2");
+};
 
+const middleLogs = (cb) => {
+  setTimeout(() => {
+    console.log("log 3");
+    cb()
+  }, 3000);
+};
 
+const lastLogs = () => {
+  console.log("log 4");
+  console.log("log 5");
+};
 
+firstLogs()
+middleLogs(lastLogs)
