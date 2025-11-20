c400297a98e5da0aa724636fbe97685d9a12f16e
Amir <amiiraliborji1388@gmail.com>
2025-11-20 10:12:15 +0330

    .

--- JavaScript/document.js
+++ JavaScript/document.js
@@ -2211,3 +2211,27 @@ const double = (n) => {
 }
 // یا
 const double = n => n * 2
+
+
+
+//* Callback functions
+
+const firstLogs = () => {
+   console.log("log 1");
+   console.log("log 2");
+ };
+ 
+ const middleLogs = (cb) => {
+   setTimeout(() => {
+     console.log("log 3");
+     cb()
+   }, 3000);
+ };
+ 
+ const lastLogs = () => {
+   console.log("log 4");
+   console.log("log 5");
+ };
+ 
+ firstLogs()
+ middleLogs(lastLogs)
